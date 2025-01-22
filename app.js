46e550ef91b89ca1517859c4149662dc27d89f75
matiasJ25 <fabianjeyma@gmail.com>
2025-01-22 20:11:20 -0300

    Agregar script del desario en cosola en app.js

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// Desafio alura - Amigo invisible
+
+//declaracion de variables
+let nombres = [];
+let sorteo = 0;
+
+//Funcion para agregar un nombre a la lista
+
+function agregarNombre() {
+    let nombre = prompt("Ingrese un nombre");
+    nombres.push(nombre);
+}
+
+//Funcion para sortear un nombre
+function sortearNombre() {
+    let indiceSorteo = Math.floor(Math.random() * nombres.length);
+    alert("El nombre elegido es: " + nombres[indiceSorteo]);
+}
+//rama principal de la aplicacion
+while (sorteo==0) {
+    agregarNombre();
+    sorteo = prompt("Para agregar otro nombre presione 0 , sino preciono cualquier tecla");
+}
+
+sortearNombre();
